d61ddc9b902db15eec4a7eee14aa94a6afa0111a
jonghyeok <bjh3593@hanyang.ac.kr>
2023-11-01 15:02:30 +0900

    231018 3교시

--- 07. seoul_subway_crowded.R
+++ 07. seoul_subway_crowded.R
@@ -34,21 +34,26 @@ ggplot(crowded1, aes(y=s0530))+
 
 summary(crowded1$s0530)
 
-#1.지하철역의 하루 평균 혼잡도
+#파생변수
+#호선
+table(crowded1$line)
+mean(crowded1$line)
+
+
+#2-1.지하철역의 하루 평균 혼잡도
 crowded1$day_mean <-
-  rowMeans(crowded1[,c('s0530','s0600','s0630','s0700','s0730','s0800','s0830','s0900','s0930','s1000','s1030','s1
-100','s1130','s1200','s1230','s1300','s1330','s1400','s1430','s1500','s1530','s1600','s1630','s1700','s1730','s1800','
-s1830','s1900','s1930','s2000','s2030','s2100','s2130','s2200','s2230','s2300','s2330')])
+  rowMeans(crowded1[,c('s0530','s0600','s0630','s0700','s0730','s0800','s0830','s0900','s0930','s1000','s1030','s1100','s1130','s1200','s1230','s1300','s1330','s1400','s1430','s1500','s1530','s1600','s1630','s1700','s1730','s1800','s1830','s1900','s1930','s2000','s2030','s2100','s2130','s2200','s2230','s2300','s2330')])
+crowded1%>%
+  summarise(mean(crowded1$day_mean))
 
-#2.지하철 호선별 하루평균 혼잡도
-#2-1.지하철 호선별 출근시간(07:00~09:00)대의 평균혼잡도
+#2-2.지하철 호선별 하루평균 혼잡도
+#2-3.지하철 호선별 출근시간(07:00~09:00)대의 평균혼잡도
 
 
 
-#3.08시 지하철 혼잡도 범주화/범주별 빈도분석
+#2-4.08시 지하철 혼잡도 범주화/범주별 빈도분석
 crowded1 %>% 
-  mutate(s80_grade=ifelse(s0800<=80, "good", ifelse(s0800<=130, "normal", ifelse(s0800<=150, "caution", 
-                                                                                 "bad"))))%>% 
+  mutate(s80_grade=ifelse(s0800<=80, "good", ifelse(s0800<=130, "normal", ifelse(s0800<=150, "caution", "bad"))))%>% 
   group_by(s80_grade) %>% 
   summarise(n=n())%>% 
   mutate(total=sum(n), pct=round(n/total*100,1))%>% 
@@ -57,8 +62,7 @@ crowded1 %>%
 
 #3-1. 호선별로 08시 지하철 혼잡도 범주화
 crowded1 %>% 
-  mutate(s80_grade=ifelse(s0800<=80, "good", ifelse(s0800<=130, "normal", ifelse(s0800<=150, "caution", 
-                                                                                 "bad"))))%>% 
+  mutate(s80_grade=ifelse(s0800<=80, "good", ifelse(s0800<=130, "normal", ifelse(s0800<=150, "caution", "bad"))))%>% 
   group_by(line, s80_grade) %>% 
   summarise(n=n())%>% 
   mutate(total=sum(n), pct=round(n/total*100,1))%>% 
